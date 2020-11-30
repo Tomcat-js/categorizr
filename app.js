d249eaa6cd534b4d41ae77423101f36c0cfba971
Tom <thomascatchlove@gmail.com>
2020-11-30 13:40:12 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// console.log("category");
+
+const a = ['a1','a2','a3','a4','a5','a6','a7','a8'];
+const b = ['b1','b2','b3','b4','b5','b6','b7','b8'];
+const c = ['c1','c2','c3','c4','c5','c6','c7','c8'];
+const d = ['d1','d2','d3','d4','d5','d6','d7','d8'];
+const e = ['e1','e2','e3','e4','e5','e6','e7','e8'];
+const f = ['f1','f2','f3','f4','f5','f6','f7','f8'];
+const g = ['g1','g2','g3','g4','g5','g6','g7','g8'];
+
+const squareNums = ['.one', '.two', '.three', '.four', '.five', '.six', '.seven','.eight']
+
+const allCategories = [a, b, c, d, e, f, g];
+
+
+const populateGrid = () => {
+    // squareNums.forEach((num, i) => {
+    //     document.querySelector(num).innerHTML = a[i];
+    // })
+
+    let usedImages = []
+    const targetCategoryIdx = Math.floor(Math.random() * allCategories.length);
+    const targetCategory = allCategories[targetCategoryIdx]
+    // console.log(targetCategory)
+
+    for (let i = 0; i < 3; i++) {
+        let randomIdx = Math.floor(Math.random() * 7);
+        // console.log(randomIdx);
+
+        if (!usedImages.includes(randomIdx)) {
+            usedImages.push(randomIdx);
+            document.querySelector(squareNums[randomIdx]).innerHTML = targetCategory[i];        
+            // console.log(usedImages);
+
+        } else {
+            i--
+        }
+        
+    }
+
+
+}
+
+populateGrid();
